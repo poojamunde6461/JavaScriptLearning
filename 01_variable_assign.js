f8c994d20d1f9809592dcda0608ea405bd352695
poojamunde6461 <118867374+poojamunde6461@users.noreply.github.com>
2022-12-08 19:59:41 +0530

    Assignment variable Declaration and Initialization
    Assignment variable Declaration and Initialization

---
+++ 01_variable_assign.js
@@ -0,0 +1,20 @@
+var myFullName;
+console.log(myFullName);
+
+var myFullName ="Pooja Munde";
+console.log(myFullName);
+
+myFullName ="Pooja S Munde";
+console.log(myFullName);          // variable update
+
+var myLove ="I Love Only JS";
+console.log(myLove);
+
+var firstName ="Pooja";
+var lastName ="Munde";
+var age ="27";
+var address ="Pune";
+var pin_code ="411057";
+var gender ="Female";
+var is_married ="No";
+console.log(firstName,lastName,age,address,pin_code,gender,is_married);
