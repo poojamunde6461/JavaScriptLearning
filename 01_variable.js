126201d1ec6237aec0106a5e7c9a5621ba02017a
poojamunde6461 <118867374+poojamunde6461@users.noreply.github.com>
2022-12-08 19:58:12 +0530

    learn today variable declaration and initialization
    learn today variable declaration and initialization

---
+++ 01_variable.js
@@ -0,0 +1,9 @@
+var myName;       // Variable Declaration
+myName ="Pooja";  // variable initialization
+console.log(myName);
+
+var myFrdName ="Krishna";  // Variable Declaration and initialization 
+console.log(myFrdName);
+
+ myFrdName ="MS Dhoni";   // Variable Update
+ console.log(myFrdName);
