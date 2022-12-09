9ccb4d69a9a9c61238d2f2c6f3e2f74f839d1d80
poojamunde6461 <118867374+poojamunde6461@users.noreply.github.com>
2022-12-09 19:19:47 +0530

    Correct Quotation Mark
    Correct Quotation Mark

--- 01_variable_assign.js
+++ 01_variable_assign.js
@@ -1,7 +1,7 @@
 var myFullName;
 console.log(myFullName);
 
-var myFullName ="Pooja Munde";
+var myFullName ="Pooja Munde";   
 console.log(myFullName);
 
 myFullName ="Pooja S Munde";
@@ -12,9 +12,9 @@ console.log(myLove);
 
 var firstName ="Pooja";
 var lastName ="Munde";
-var age ="27";
+var age =27;
 var address ="Pune";
-var pin_code ="411057";
+var pin_code =411057;
 var gender ="Female";
 var is_married ="No";
 console.log(firstName,lastName,age,address,pin_code,gender,is_married);
