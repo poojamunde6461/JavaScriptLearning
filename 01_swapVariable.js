61d932899612ab1399f75ed159f6b6f009803c81
poojamunde6461 <118867374+poojamunde6461@users.noreply.github.com>
2022-12-09 19:13:14 +0530

    Learning typeOf And Swap Variable
    Learning typeOf And Swap Variable

---
+++ 01_swapVariable.js
@@ -0,0 +1,13 @@
+console.log("=================================== Assignment No. 1 =========================================");
+var myNumber =200;
+var yourNumber =300;
+console.log("Before Swap");
+console.log(myNumber, yourNumber);
+
+var temp = myNumber;
+myNumber = yourNumber;
+yourNumber = temp;
+console.log("Afer Swap");
+console.log(myNumber, yourNumber);
+
+
