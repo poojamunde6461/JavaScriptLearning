109ae307d2ced1e3c3f14fdc51475d37b0391067
poojamunde6461 <118867374+poojamunde6461@users.noreply.github.com>
2022-12-09 19:14:23 +0530

    Assignment typeOf And Swap Variable
    Assignment typeOf And Swap Variable

---
+++ 01_swap_var_assign.js
@@ -0,0 +1,27 @@
+console.log("=================================== Assignment No. 1 =========================================");
+console.log("1.Swap two variables........................");
+var sweety ="Sweety";
+var cutie ="Cutie";
+console.log("Before Swap");
+console.log(sweety,cutie);
+
+var temp = "sweety";
+sweety = cutie;
+cutie = temp;
+console.log("Afer Swap");
+console.log(sweety,cutie);
+
+
+console.log("2.Swap three variables........................");
+var num1 =100;
+var num2 =200;
+var num3 =300;
+console.log("Before Swap");
+console.log(num1,num2,num3);
+
+var temp =num1;
+num1 = num2;
+num2 = num3;
+num3 = temp;
+console.log("Afer Swap");
+console.log(num1,num2,num3);
---
+++ 01_typeOf.js
@@ -0,0 +1,39 @@
+console.log("==================================== Assignment No. 2 ===========================================");
+console.log("----------------------------------Before Initialization----------------------------------");
+var bankName;
+console.log(typeof bankName);
+console.log(bankName);
+
+console.log("----------------------------------After Initialization----------------------------------");
+var bankName = "SBI Bank";
+console.log(bankName);
+console.log(typeof bankName);
+console.log("=================================================================================================");
+
+var marks = "90%";
+console.log(marks);
+console.log(typeof marks);
+console.log("=================================================================================================");
+
+console.log("----------------------------------Before Initialization----------------------------------");
+var isWorking;
+console.log(isWorking);
+console.log(typeof isWorking);
+
+console.log("----------------------------------After Initialization----------------------------------");
+var isWorking = true;
+console.log(isWorking);
+console.log(typeof isWorking);
+
+console.log("=================================================================================================");
+
+var totalCount = "Hundred and Seven";
+console.log(totalCount);
+console.log(typeof totalCount);
+
+console.log("------------------------------------------------------------------------------------------");
+
+var totalCount = 107;
+console.log(totalCount);
+console.log(typeof totalCount);
+
