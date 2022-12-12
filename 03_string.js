34260a727bb1e39813f5c3683afe91acb7b38a21
poojamunde6461 <118867374+poojamunde6461@users.noreply.github.com>
2022-12-12 20:31:58 +0530

    Some changes in String
    Some changes in String

--- 03_string.js
+++ 03_string.js
@@ -3,7 +3,8 @@ console.log("Given String is:",greet);
 
 console.log("Total Character in the string is:",greet.length);
 
-console.log("Char at index 3:", greet.charAt(5));
+console.log("Char at index 5:", greet.charAt(5));
+console.log("Char at index 7:", greet.charAt(7));
 
 var total = greet.length;
 console.log("Character at Last index:",greet.charAt(total-1));
