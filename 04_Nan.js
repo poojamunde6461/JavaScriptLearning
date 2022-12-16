e5f1b8bd301b399ec1d65d0d0e8b2670def9a18b
poojamunde6461 <118867374+poojamunde6461@users.noreply.github.com>
2022-12-16 16:07:55 +0530

    Learn yesterday Nan And Ternary Operator
    Learn yesterday Nan And Ternary Operator

---
+++ 04_Nan.js
@@ -0,0 +1,15 @@
+var resultDivide = 0/0;
+console.log(`Result is: ${resultDivide}`);
+console.log(`Type of${resultDivide} is: ${typeof resultDivide}`);
+
+var resultAdd = resultDivide + 10;
+console.log(`ResultAdd is: ${resultAdd}`);
+
+var resultMulti = resultDivide * 10;
+console.log(`Result Multiplication is: ${resultMulti}`);
+
+var num = new Number("Prathamesh"); 
+console.log(`num is: ${num}`);
+
+var num = new Number("100"); 
+console.log(`num is: ${num}`);
